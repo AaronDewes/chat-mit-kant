9f0b5ca4892b6ac8d3ad75112eefabcbfa07efb0
Aaron Dewes <aaron.dewes@protonmail.com>
2024-04-17 19:21:43 +0200

    Fixup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ["@nuxt/ui", "nuxt-security"],
   runtimeConfig: {
     apiKey: process.env.DELPHI_API_KEY,
   },
   colorMode: {
-    preference: 'dark'
+    preference: "dark",
+  },
+  security: {
+    headers: { contentSecurityPolicy: false, crossOriginEmbedderPolicy: "require-corp" },
   }
-})
+});
