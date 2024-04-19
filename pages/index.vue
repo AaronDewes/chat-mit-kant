0c62d8e08e7794f058d470e79833d6f32053e40f
Aaron Dewes <aaron.dewes@protonmail.com>
2024-04-19 10:47:48 +0200

    Loader

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <img src="~/assets/badge_300_jahre_kant.png" class="w-48 mb-4">
       <h1 class="text-4xl font-bold">Immanuel Kant</h1>
       <h3 class="text-2xl mb-4">Chatte live mit dem berühmten Philosophen als KI.</h3>
-      <UButton @click="startConversation" size="xl" icon="i-heroicons-chat-bubble-left">Gespräch beginnen</UButton>
+      <UButton @click="startConversation" size="xl" icon="i-heroicons-chat-bubble-left" :loading>Gespräch beginnen</UButton>
     </div>
     <span class="mt-auto mb-3 text-sm">Ein Projekt von Aaron Dewes - ermöglicht von
       <a href="https://delphi.ai" class="text-blue-500">delphi.ai</a>.
@@ -13,7 +13,10 @@
 </template>
 
 <script setup lang="ts">
+const loading = ref(false);
+
 async function startConversation() {
+  loading.value = true;
   const data = await $fetch("/api/conversation", {
     method: "POST"
   });
