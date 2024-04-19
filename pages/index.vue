800d8d7d8b5fc8eedb6d2a962d0ee587b26f6063
Aaron Dewes <aaron.dewes@protonmail.com>
2024-04-19 10:50:54 +0200

    Redesign footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="w-screen h-screen flex flex-col gap-2 items-center justify-center" id="main">
-    <div class="kant-circle flex text-center justify-center items-center flex-col mt-auto">
-      <img src="~/assets/badge_300_jahre_kant.png" class="w-48 mb-4">
+  <div
+    class="w-screen h-screen flex flex-col gap-2 items-center justify-center"
+    id="main"
+  >
+    <div
+      class="kant-circle flex text-center justify-center items-center flex-col mt-auto"
+    >
+      <img src="~/assets/badge_300_jahre_kant.png" class="w-48 mb-4" />
       <h1 class="text-4xl font-bold">Immanuel Kant</h1>
-      <h3 class="text-2xl mb-4">Chatte live mit dem berühmten Philosophen als KI.</h3>
-      <UButton @click="startConversation" size="xl" icon="i-heroicons-chat-bubble-left" :loading>Gespräch beginnen</UButton>
+      <h3 class="text-2xl mb-4">
+        Chatte live mit dem berühmten Philosophen als KI.
+      </h3>
+      <UButton
+        @click="startConversation"
+        size="xl"
+        icon="i-heroicons-chat-bubble-left"
+        :loading
+        >Gespräch beginnen</UButton
+      >
     </div>
-    <span class="mt-auto mb-3 text-sm">Ein Projekt von Aaron Dewes - ermöglicht von
-      <a href="https://delphi.ai" class="text-blue-500">delphi.ai</a>.
+    <span class="mt-auto mb-3 text-lg bg-slate-700/50 p-4 rounded-lg"
+      >Ein Projekt von Aaron Dewes - ermöglicht von <DelphiLogo class="h-3 inline mr-1 my-auto" /><a
+        href="https://delphi.ai"
+        class="text-blue-500"
+        >delphi.ai</a
+      >.
     </span>
   </div>
 </template>
@@ -18,7 +35,7 @@ const loading = ref(false);
 async function startConversation() {
   loading.value = true;
   const data = await $fetch("/api/conversation", {
-    method: "POST"
+    method: "POST",
   });
   const newId = data.new.conversation_id;
   await navigateTo(`/conversation/${newId}`);
